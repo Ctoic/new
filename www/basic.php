a2f682212ffc0872edf7fccad1c0d2c8dee870e5
ctoic <i202612@nu.edu.pk>
2024-07-16 19:41:02 +0500

    my php helloworld

---
+++ www/basic.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>basic</title>
+</head>
+<body>
+    <?php 
+        echo"hel";
+        ecHo "hello";
+        Echo "halal";
+
+        // now using operatiors with a variable 
+        $sum = 3+4;
+        echo "sum";
+
+    ?>
+
+    // YOU CAN WRITE COMMENT LIKE C++
+    # ALSO LIKE PYTHON 
+    /* THIS IS MULTIPLE LINE. 
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque odio nobis ipsum quas in temporibus, alias omnis ducimus, sunt quisquam inventore itaque, iste voluptates provident libero animi eius accusantium dignissimos.
+    
+    */
+
+    
+</body>
+</html>
